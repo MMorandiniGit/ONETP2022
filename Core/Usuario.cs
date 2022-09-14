717332bf2b2731b9d82f5eebd17a6d3d424416cc
Martín Morandini <84515114+MMorandiniGit@users.noreply.github.com>
2022-09-14 09:06:58 -0300

    Creada PersistenciaDB

--- Core/Usuario.cs
+++ Core/Usuario.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Usuario")]
 public class Usuario
 {
+    [Key]
+    public int Id { get; set; }
+    [StringLength(45)]
     public string? Nombre { get; set; }
+    [StringLength(45)]
     public string? Apellido { get; set; }
+    [StringLength(45)]
     public string? email { get; set; }
 }
---
+++ Persistencia/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistencia
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            if (!ob.IsConfigured)
+            {
+                IConfiguration myConfig = new ConfigurationBuilder()
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile("appSettings.json")
+                    .Build();
+
+                string strConexion = myConfig.GetConnectionString("dev");
+                var serverVersion = new MySqlServerVersion(versionString: myConfig["SerVersion"]);
+                ob.UseMySql(strConexion, serverVersion);
+            }
+        }
+    }
+}
