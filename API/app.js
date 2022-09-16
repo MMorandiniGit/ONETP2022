6a6c7cc02fb4c25516f0abed3a652baf598092ab
LucasMedina04 <medina.lucas.e@gmail.com>
2022-09-16 10:19:00 -0300

    :car:

--- API/app.js
+++ API/app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views', './views') 
+app.set('view engine', 'ejs') 
 
+app.use(express.static(__dirname + '/public'));
 
+app.get("/", (pedido, respuesta) => {
+    respuesta.render('index')
+})
 
+app.get("/formulario", (pedido, respuesta) => {
+    respuesta.render('formulario')
+})
 
 app.listen(port)
